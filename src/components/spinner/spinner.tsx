93659d7b87dc2ed4b41088e531af46566a81420f
tomilin229 <tomilin229@gmail.com>
2023-11-13 01:50:57 +0300

    7.14. Истина где-то на сервере

---
+++ src/components/spinner/spinner.tsx
@@ -0,0 +1,8 @@
+import './Spinner.scss';
+
+export const Spinner = () => (
+  <div className="spinner-container">
+    <div className="spinner"></div>
+  </div>
+);
+
--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,16 +1,40 @@
-import {FC} from 'react';
+import { FC, useEffect } from 'react';
 import { Footer } from '../../components/footer/footer.tsx';
-import { films } from '../../mocks/films.ts';
 import { FilmCard } from '../../components/film-card/film-card.tsx';
 import { Catalog } from '../../components/catalog/catalog.tsx';
 
-export const Main: FC = () => (<>
-  <FilmCard film={films[0]} />
+import { useAppDispatch, useAppSelector } from '../../hooks/store.ts';
+import { fetchMovies } from '../../store/action.ts';
+import { selectFilmsData, selectFilmsError, selectFilmsStatus } from '../../store/reducer.ts';
+import { Spinner } from '../../components/spinner/spinner.tsx';
+import { Page404 } from '../page-404/page-404.tsx';
 
-  <div className="page-content">
-    <Catalog withGenres />
-    <Footer/>
-  </div>
-</>
+export const Main: FC = () => {
+  const dispatch = useAppDispatch();
+  const films = useAppSelector(selectFilmsData);
+  const filmsError = useAppSelector(selectFilmsError);
+  const filmsStatus = useAppSelector(selectFilmsStatus);
 
-);
+  useEffect(() => {
+    if (films === null) {
+      dispatch(fetchMovies());
+    }
+  }, [dispatch, films]);
+
+  if (filmsError) {
+    return <Page404/>;
+  }
+
+  if (!films || filmsStatus === 'LOADING') {
+    return <Spinner/>;
+  }
+
+  return (<>
+    <FilmCard film={films[0]} />
+
+    <div className="page-content">
+      <Catalog withGenres />
+      <Footer/>
+    </div>
+  </>);
+};
